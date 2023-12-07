8bceb0dbe62390b1dd937b31c04e02cfbfdac336
noname068218 <128069790+noname068218@users.noreply.github.com>
2023-12-07 20:18:56 +0100

    big update

--- src/components/MovieDetalis/MovieDetails.style.js
+++ src/components/MovieDetalis/MovieDetails.style.js
@@ -1,5 +1,101 @@
 import styled from 'styled-components';
 
-export const FilmCard = styled.img`
-  width: 300px;
+export const Card = styled.div`
+  width: 800px;
+  height: 400px;
+  background: transparent;
+  position: relative;
+  top: 0;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  top: 0;
+  bottom: 0;
+  border-radius: 10px;
+  -webkit-border-radius: 10px;
+  -moz-border-radius: 10px;
+  box-shadow: 0px 20px 30px 3px rgba(0, 0, 0, 0.55);
+  display: flex;
+  flex-direction: row;
+  overflow: hidden;
+`;
+
+export const CardLeft = styled.div`
+  width: 40%;
+  height: 100%;
+  overflow: hidden;
+  background: transparent;
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 10px 0 0 10px;
+    -webkit-border-radius: 10px 0 0 10px;
+    -moz-border-radius: 10px 0 0 10px;
+  }
+`;
+
+export const CardRight = styled.div`
+  width: 60%;
+  background: #000000;
+  height: auto;
+  border-radius: 0 10px 10px 0;
+  -webkit-border-radius: 0 10px 10px 0;
+  -moz-border-radius: 0 10px 10px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  h2 {
+    color: white;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 400;
+    text-align: center;
+    font-size: 40px;
+    margin-top: 0;
+    margin-bottom: 0;
+
+    padding: 0 0 0 40px;
+    letter-spacing: 1px;
+  }
+  p {
+    text-align: center;
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+`;
+
+export const Gener = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 0;
+  margin-bottom: 0;
+`;
+
+export const TextGenerContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 0;
+  margin-bottom: 0;
+`;
+
+//CAST Reviews
+
+export const ListContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+export const List = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  gap: 20px;
+`;
+
+export const ListItem = styled.li`
+  margin: 0;
 `;
--- src/components/pages/MovieDetails.js
+++ src/components/pages/MovieDetails.js
@@ -1,7 +1,16 @@
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import { Loader } from '../Animations/LoadingSpiner';
 import { useDataFetching } from 'components/hooks';
-import { FilmCard } from 'components/MovieDetalis/MovieDetails.style';
+import {
+  Card,
+  CardLeft,
+  CardRight,
+  Gener,
+  TextGenerContainer,
+  ListItem,
+  ListContainer,
+  List,
+} from 'components/MovieDetalis/MovieDetails.style';
 import { useRef } from 'react';
 
 const MovieDetails = () => {
@@ -9,43 +18,58 @@ const MovieDetails = () => {
     useDataFetching('movieDetails');
   const location = useLocation();
   const backLinkRef = useRef(location);
+
+  const goHomePath = backLinkRef.current?.state?.from ?? '/';
+
   return (
     <div>
       {isMovieDetailsLoading ? (
         <Loader />
       ) : (
         <div>
-          <Link to={backLinkRef.current.state?.from ?? '/'}>
+          <Link to={goHomePath}>
             <button>Go Home</button>
           </Link>
+          <Card>
+            <CardLeft>
+              {movieDetails.backdrop_path ? (
+                <img
+                  src={`https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`}
+                  alt={movieDetails.title}
+                />
+              ) : (
+                <span>No Image Available</span>
+              )}
+            </CardLeft>
+            <CardRight>
+              <h2>{movieDetails.title}</h2>
+              <p>{movieDetails.overview}</p>
 
-          <h2>{movieDetails.title}</h2>
-          <p>{movieDetails.overview}</p>
-          <h2>Gener</h2>
-          {movieDetails.genres &&
-            movieDetails.genres.map(gener => (
-              <p key={gener.id}>{gener.name}</p>
-            ))}
-          {movieDetails.backdrop_path ? (
-            <FilmCard
-              src={`https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`}
-              alt={movieDetails.title}
-            />
-          ) : (
-            <span>No Image Available</span>
-          )}
-
-          <ul>
-            <li>
-              <Link to="cast">Cast</Link>
-            </li>
-            <li>
-              <Link to="reviews">Reviews</Link>
-            </li>
-          </ul>
-          <Outlet />
+              <Gener>Gener</Gener>
+              <TextGenerContainer>
+                {movieDetails.genres &&
+                  movieDetails.genres.map(gener => (
+                    <p key={gener.id}>{gener.name}</p>
+                  ))}
+              </TextGenerContainer>
+            </CardRight>
+          </Card>
         </div>
       )}
+
+      <div>
+        <ListContainer>
+          <List>
+            <ListItem>
+              <Link to="cast">Cast</Link>
+            </ListItem>
+            <ListItem>
+              <Link to="reviews">Reviews</Link>
+            </ListItem>
+          </List>
+        </ListContainer>
+        <Outlet />
+      </div>
     </div>
   );
 };
