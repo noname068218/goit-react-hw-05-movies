452d7e873226a39649d45d27461325251a88d515
noname068218 <128069790+noname068218@users.noreply.github.com>
2023-12-08 22:33:50 +0000

    Deploying to gh-pages from @ noname068218/goit-react-hw-05-movies@5b70bde2d3b214aeaf01e51e8d6b00a42c359e6e 🚀

---
+++ static/js/922.ff9784c3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[922],{922:(e,t,r)=>{r.r(t),r.d(t,{default:()=>c});var o=r(87),a=r(184);function c(){return(0,a.jsxs)("div",{children:["go home ",(0,a.jsx)(o.rU,{to:"/",children:"NotFound"})]})}}}]);
+//# sourceMappingURL=922.ff9784c3.chunk.js.map
